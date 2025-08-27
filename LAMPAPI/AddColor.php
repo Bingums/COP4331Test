136102e31ceab6c5ed71de1badc1fcbb747dda68
Diego Van Eggelpoel <150079765+Bingums@users.noreply.github.com>
2025-08-27 14:09:15 -0400

    Updated login

--- LAMPAPI/AddColor.php
+++ LAMPAPI/AddColor.php
@@ -4,7 +4,7 @@
 	$color = $inData["color"];
 	$userId = $inData["userId"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli("localhost", "DiegoV", "meUV5osj?", "COP4331");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
